ea7bdad8ffcdb7ae79d58627bed2bb327172fdad
ghazals <ghsaheb@gmail.com>
2018-05-04 20:33:59 +0430

    createJWT method added

--- src/main/java/JWTHandler.java
+++ src/main/java/JWTHandler.java
@@ -1,6 +1,13 @@
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.io.UnsupportedEncodingException;
+import java.security.Key;
+import io.jsonwebtoken.*;
+import java.util.Date;
 
 public class JWTHandler {
-    private String createJWT(String id, String issuer, String subject, long ttlMillis) {
+    //Sample method to construct a JWT
+    private static String createJWT(String issuer, String subject) {
 
         //The JWT signature algorithm we will be using to sign the token
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
@@ -8,25 +15,25 @@ public class JWTHandler {
         long nowMillis = System.currentTimeMillis();
         Date now = new Date(nowMillis);
 
-        //We will sign our JWT with our ApiKey secret
-        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(apiKey.getSecret());
-        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
-
         //Let's set the JWT Claims
-        JwtBuilder builder = Jwts.builder().setId(id)
-                .setIssuedAt(now)
-                .setSubject(subject)
-                .setIssuer(issuer)
-                .signWith(signatureAlgorithm, signingKey);
-
-        //if it has been specified, let's add the expiration
-        if (ttlMillis >= 0) {
-            long expMillis = nowMillis + ttlMillis;
-            Date exp = new Date(expMillis);
-            builder.setExpiration(exp);
+        JwtBuilder builder = null;
+        try {
+            builder = Jwts.builder()
+                    .setIssuedAt(now)
+                    .setSubject(subject)
+                    .setIssuer(issuer)
+                    .signWith(signatureAlgorithm, "mozi-amoo".getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         }
 
         //Builds the JWT and serializes it to a compact, URL-safe string
         return builder.compact();
+    }
+
+    public static void main(String[] args) {
+        String jwt = createJWT("GVRE","ghaz");
+        System.out.println("jwt:"+jwt);
+
     }
 }
